08edc6f770ae7b2107c49985db4ff24c5dfad410
ConnorTimpe <46509018+ConnorTimpe@users.noreply.github.com>
2020-01-28 13:21:02 -0600

    Created log in function and linked to main screen.

--- AppScheduler/src/Controller/LogInScreenController.java
+++ AppScheduler/src/Controller/LogInScreenController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -28,6 +29,8 @@ public class LogInScreenController implements Initializable {
 
     Stage stage;
     Parent scene;
+    private final String USERNAME = "test";
+    private final String PASSWORD = "test";
 
     @FXML
     private Label UsernameLabel;
@@ -50,20 +53,20 @@ public class LogInScreenController implements Initializable {
     @FXML
     void OnActionLogIn(ActionEvent event) throws IOException {
         if (validCredentials()) {
-            System.out.println("logged in");
+            connectToDatabase();
             goToMainScreen(event);
         } else {
-
+            createErrorMessage();
         }
-
-        //Test username / password
-        //connect to database
-        //Change screens
-        //error message if info is wrong
     }
 
     private boolean validCredentials() {
-        return true;
+        return LogInUsernameText.getText().equals(USERNAME)
+                && LogInPasswordText.getText().equals(PASSWORD);
+    }
+
+    private void connectToDatabase() {
+        System.out.println("Connected");
     }
 
     private void goToMainScreen(ActionEvent event) throws IOException {
@@ -77,9 +80,15 @@ public class LogInScreenController implements Initializable {
         stage.setResizable(false);
         stage.show();
     }
-    
-    private void createErrorMessage(String alertTitle, String alertContent){
-        
+
+    private void createErrorMessage() {
+        String alertTitle = "Error"; //loginScreenRB.getString("alertTitle")
+        String alertContent = "Incorrect login info"; //loginScreenRB.getString("alertContent")
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(alertTitle);
+        alert.setContentText(alertContent);
+        alert.showAndWait();
     }
 
     /**
