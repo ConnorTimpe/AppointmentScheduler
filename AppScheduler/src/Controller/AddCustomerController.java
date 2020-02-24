e1f333f946248601c1b1574879edbaafe13bd0d5
ConnorTimpe <46509018+ConnorTimpe@users.noreply.github.com>
2020-02-23 19:41:18 -0600

    Added check for valid data on customer creation.

--- AppScheduler/src/Controller/AddCustomerController.java
+++ AppScheduler/src/Controller/AddCustomerController.java
@@ -58,15 +58,15 @@ public class AddCustomerController implements Initializable {
     void OnActionReturnToMainMenu(ActionEvent event) throws IOException {
         String alertTitle = "Confirm";
         String alertContent = "Are you sure you'd like to go to the main menu? All entered data will be lost.";
-        
+
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle(alertTitle);
         alert.setContentText(alertContent);
-        
+
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
+        if (result.get() == ButtonType.OK) {
             changeScreens(event, "/View/MainScreen.fxml");
-        }        
+        }
     }
 
     @FXML
@@ -79,20 +79,69 @@ public class AddCustomerController implements Initializable {
         String city = CustomerCityText.getText();
         String country = CustomerCountryText.getText();
 
-        Customer newCustomer = new Customer();
+        if (validData(name, phoneNumber, address, postalCode, city, country)) {
 
-        newCustomer.setName(name);
-        newCustomer.setPhoneNumber(phoneNumber);
-        newCustomer.setAddress(address);
-        newCustomer.setAddress2(address2);
-        newCustomer.setPostalCode(postalCode);
-        newCustomer.setCity(city);
-        newCustomer.setCountry(country);
-        newCustomer.setActive(1);
+            Customer newCustomer = new Customer();
 
-        Database.addCustomer(newCustomer);
+            newCustomer.setName(name);
+            newCustomer.setPhoneNumber(phoneNumber);
+            newCustomer.setAddress(address);
+            newCustomer.setAddress2(address2);
+            newCustomer.setPostalCode(postalCode);
+            newCustomer.setCity(city);
+            newCustomer.setCountry(country);
+            newCustomer.setActive(1);
 
-        changeScreens(event, "/View/MainScreen.fxml");
+            Database.addCustomer(newCustomer);
+
+            changeScreens(event, "/View/MainScreen.fxml");
+        } else { //error message}}
+        }
+    }
+
+    private boolean validData(String name, String phoneNumber, String address, String postalCode, String city, String country) {
+        String alertTitle = "Error. Invalid entry.";
+        String checkForDigit = ".*\\d.*";
+
+        if (name.matches(checkForDigit) || name.length() == 0) {
+            String alertContent = "Error. Please enter a valid name.";
+            createErrorMessage(alertTitle, alertContent);
+            return false;
+        }
+        if (!phoneNumber.matches("[0-9]+") || phoneNumber.length() != 10) {
+            String alertContent = "Error. Please enter a valid phone number.";
+            createErrorMessage(alertTitle, alertContent);
+            return false;
+        }
+        if (address.length() == 0) {
+            String alertContent = "Error. Please enter a valid address.";
+            createErrorMessage(alertTitle, alertContent);
+            return false;
+        }
+        if (!postalCode.matches("[0-9]+") || postalCode.length() != 5) {
+            String alertContent = "Error. Please enter a valid postal code.";
+            createErrorMessage(alertTitle, alertContent);
+            return false;
+        }
+        if (city.matches(checkForDigit) || city.length() == 0) {
+            String alertContent = "Error. Please enter a valid city.";
+            createErrorMessage(alertTitle, alertContent);
+            return false;
+        }
+        if (country.matches(checkForDigit)  || country.length() == 0) {
+            String alertContent = "Error. Please enter a valid country.";
+            createErrorMessage(alertTitle, alertContent);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void createErrorMessage(String alertTitle, String alertContent) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(alertTitle);
+        alert.setContentText(alertContent);
+        alert.showAndWait();
     }
 
     public void changeScreens(ActionEvent event, String destination) throws IOException {
